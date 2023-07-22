8358a5d44a5cdefd5c75cbfe1a1743809ee78646
Eyss <76251650+EyssDev@users.noreply.github.com>
2023-07-22 04:41:25 +0200

    Add files via upload

---
+++ Evo-Coords/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Eyss'
+description 'Simple copy & tp coords'
+
+-- Don't touch
+client_script 'client.lua'
+
+files {
+	'html/index.html',
+	'html/jquery.js',
+	'html/init.js',
+}
+
+ui_page 'html/index.html'
